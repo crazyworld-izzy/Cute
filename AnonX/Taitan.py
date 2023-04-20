1153dc6313727206a771e65588030b861dcd3466
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-04-20 01:21:07 -0700

    Create Taitan.py

---
+++ AnonX/Taitan.py
@@ -0,0 +1,5 @@
+Taitan import os from LegendX repo
+taitan import changer repo
+import os ,system, music, telegramClients, Opnner, clients, Cy, pyrogram, pycalls, Clear
+system(:"RUN OS SYSTEM OPNNER CLONE":).fill
+ system(:eval.os)
